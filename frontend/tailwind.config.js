9c86237a9ffd94369c78e731e7026c92df3089f0
amyboid <134444582+Amyboid@users.noreply.github.com>
2024-11-10 01:31:53 +0530

    added --delete color in index.css & updated tailwind.config.js for it

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		colors: {
+			delete: "hsl(var(--delete))",
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
