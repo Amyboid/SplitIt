8fba1beaf3614a578413c2ca9eac496b8eb54434
amyboid <134444582+Amyboid@users.noreply.github.com>
2024-11-10 01:44:07 +0530

    updated groupform accepts user array with validation

--- frontend/src/views/Groups.tsx
+++ frontend/src/views/Groups.tsx
@@ -10,10 +10,15 @@ import {
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import * as z from "zod";
 import { Textarea } from "@/components/ui/textarea";
-import {  SearchBox } from "@/components/search-box";
+import { SearchBox } from "@/components/search-box";
+import { Trash2 } from "lucide-react";
+
+const userObj = z.object({
+  name: z.string(),
+});
 
 const formSchema = z.object({
   groupname: z.string().min(3, {
@@ -25,34 +30,42 @@ const formSchema = z.object({
       message: "Description must be in the range of 30 characters.",
     })
     .or(z.literal("")),
-    groupmembers: z
-    .string()
-    .max(30, {
-      message: "Description must be in the range of 30 characters.",
-    })
-    .or(z.literal("")),
+  groupmembers: z.array(userObj).min(2),
 });
 
 function GroupForm() {
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       groupname: "",
       groupdescription: "",
-      groupmembers: "",
+      groupmembers: [],
     },
   });
+
+  const { control, handleSubmit } = form;
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "groupmembers",
+  });
+
+  const handleAddMember = (memberName: string) => {
+    console.log("memberName: ", memberName);
+    append({ name: memberName });
+  };
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 p-3">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-8 p-3">
           <div className="flex flex-col gap-2 group-info bg-secondary p-4 rounded-md">
             <h1 className="font-semibold text-xl mb-1 ">Group Information</h1>
             <FormField
-              control={form.control}
+              control={control}
               name="groupname"
               render={({ field }) => (
                 <FormItem>
@@ -65,7 +78,7 @@ function GroupForm() {
               )}
             />
             <FormField
-              control={form.control}
+              control={control}
               name="groupdescription"
               render={({ field }) => (
                 <FormItem>
@@ -84,20 +97,28 @@ function GroupForm() {
 
           {/* members section */}
           <div className="flex flex-col gap-2 bg-secondary p-4 rounded-md">
-            <h1 className="font-semibold text-xl mb-1 ">Group Members</h1> 
+            <h1 className="font-semibold text-xl mb-1 ">Group Members</h1>
+            <div className="">
+              {fields.map((member, index) => (
+                <div key={member.id} className="rounded-md p-3 pl-5 bg-background flex justify-between mb-1">
+                  <span className="text-sm">{member.name}</span>
+                  <Trash2 className="w-5 text-delete cursor-pointer" onClick={() => remove(index)}/>
+                </div>
+              ))}
+            </div>
             <FormField
-              control={form.control}
+              control={control}
               name="groupmembers"
-              render={({ field }) => (
+              render={() => (
                 <FormItem>
-                  {/* <FormLabel>Members</FormLabel> */}
                   <FormControl>
-                    <SearchBox/>
+                    <SearchBox onAddMember={handleAddMember} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
-            /> 
+            />
+           
           </div>
           <Button type="submit">Create</Button>
         </form>
@@ -109,6 +130,7 @@ function GroupForm() {
 export default function Groups() {
   return (
     <>
+
       <GroupForm />
     </>
   );
