21f0ef29b338c9b0bdd201081c8a8c8d20306dcb
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-11-09 18:36:57 +0530

    added notification ui

--- frontend/src/views/Dashboard.tsx
+++ frontend/src/views/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { TypographyH3 } from "@/components/ui/typography-h3";
 import { Bell, Bolt } from "lucide-react";
 import WavingHandSvg from "@/assets/waving_hand_color_default.svg"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Dashboard() {
     const user = "surajit" // example user
@@ -14,6 +15,16 @@ export default function Dashboard() {
                 <Bell size={20} className="inline ml-auto mr-4 self-center" />
                 <Bolt size={20} className="inline self-center" />
             </div>
+            <Card className="mt-10 mx-auto w-[90%] overflow-hidden">
+                <div className="notification-card-background">
+                <CardHeader className="p-3 pb-2">
+                    <CardTitle className="text-sm text-muted-foreground">Notification</CardTitle>
+                </CardHeader>
+                <CardContent className="pt-0 pb-5 pl-3">
+                Automate your savings! Set aside a small percentage of your income each month.
+                </CardContent>
+                </div>
+            </Card>
         </>
     )
 }
