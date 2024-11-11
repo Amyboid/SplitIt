8fa7947566658fe6cf03eb5bde6cfd58bda2887b
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-11-12 02:26:15 +0530

    formatted and removed unused imports

--- frontend/src/views/Settings.tsx
+++ frontend/src/views/Settings.tsx
@@ -1,7 +1,7 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { TypographyH3 } from "@/components/ui/typography-h3";
-import { CircleUser, Languages, LogOut, Scale } from "lucide-react";
+import { CircleUser, Languages, Scale } from "lucide-react";
 
 export default function Settings() {
     return (
@@ -15,22 +15,22 @@ export default function Settings() {
             <div className="grid gap-4 mt-8 mx-4">
                 <Card>
                     <CardHeader className="p-4 flex flex-row items-center">
-                    <CircleUser size={20} className="mt-1 mr-4" /> <CardTitle className=" text-lg">Account</CardTitle>
+                        <CircleUser size={20} className="mt-1 mr-4" /> <CardTitle className=" text-lg">Account</CardTitle>
                     </CardHeader>
                 </Card>
                 <Card>
                     <CardHeader className="p-4 flex flex-row items-center">
-                    <ModeToggle /> <CardTitle className=" text-lg ml-9 z-10">Theme</CardTitle>
+                        <ModeToggle /> <CardTitle className=" text-lg ml-9 z-10">Theme</CardTitle>
                     </CardHeader>
                 </Card>
                 <Card>
                     <CardHeader className="p-4 flex flex-row items-center">
-                    <Languages size={20} className="mt-1 mr-4" /> <CardTitle className=" text-lg">Language</CardTitle>
+                        <Languages size={20} className="mt-1 mr-4" /> <CardTitle className=" text-lg">Language</CardTitle>
                     </CardHeader>
                 </Card>
                 <Card>
                     <CardHeader className="p-4 flex flex-row items-center">
-                    <Scale size={20} className="mt-1 mr-4" /> <CardTitle className=" text-lg">Licences</CardTitle>
+                        <Scale size={20} className="mt-1 mr-4" /> <CardTitle className=" text-lg">Licences</CardTitle>
                     </CardHeader>
                 </Card>
             </div>
