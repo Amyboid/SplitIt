6e7f08a1a2ccfbf5531467e8104712e6966fd58d
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-11-12 00:23:19 +0530

    add finnancial quick glance

--- frontend/src/views/Dashboard.tsx
+++ frontend/src/views/Dashboard.tsx
@@ -1,7 +1,8 @@
 import { TypographyH3 } from "@/components/ui/typography-h3";
-import { Bell, Bolt } from "lucide-react";
+import { Bell, Bolt, CalendarPlus, ChartLine, Mic, RefreshCcw, UserPlus, Users } from "lucide-react";
 import WavingHandSvg from "@/assets/waving_hand_color_default.svg"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default function Dashboard() {
     const user = "surajit" // example user
@@ -17,14 +18,30 @@ export default function Dashboard() {
             </div>
             <Card className="mt-10 mx-auto w-[90%] overflow-hidden">
                 <div className="notification-card-background">
-                <CardHeader className="p-3 pb-2">
-                    <CardTitle className="text-sm text-muted-foreground">Notification</CardTitle>
-                </CardHeader>
-                <CardContent className="pt-0 pb-5 pl-3">
-                Automate your savings! Set aside a small percentage of your income each month.
-                </CardContent>
+                    <CardHeader className="p-3 pb-2">
+                        <CardTitle className="text-sm text-muted-foreground">Notification</CardTitle>
+                    </CardHeader>
+                    <CardContent className="pt-0 pb-5 pl-3">
+                        Automate your savings! Set aside a small percentage of your income each month.
+                    </CardContent>
                 </div>
             </Card>
+            <div className="mt-10 mx-auto w-[90%]">
+                <div className="grid grid-cols-2 gap-4 my-8">
+                    <Card className="border-red-600">
+                        <CardHeader>
+                            <CardTitle>₹ 500000</CardTitle>
+                            <CardDescription className="text-red-600">debt</CardDescription>
+                        </CardHeader>
+                    </Card>
+                    <Card className="border-green-600">
+                        <CardHeader>
+                            <CardTitle>₹ 5000</CardTitle>
+                            <CardDescription className="text-green-600">credit</CardDescription>
+                        </CardHeader>
+                    </Card>
+                </div>
+            </div>
         </>
     )
 }
