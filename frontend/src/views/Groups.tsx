2450a481d83e689a108a7d26927a06d6f8b20c9f
amyboid <134444582+Amyboid@users.noreply.github.com>
2024-11-13 00:24:46 +0530

    updated, added separate internal component for grouplist and removed group form to different component

--- frontend/src/views/Groups.tsx
+++ frontend/src/views/Groups.tsx
@@ -1,44 +1,40 @@
-"use client";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/components/ui/button";
-import { useFieldArray, useForm } from "react-hook-form";
-import * as z from "zod";
-import { Textarea } from "@/components/ui/textarea";
-import { SearchBox } from "@/components/search-box";
-import { Fullscreen, Maximize2, Minimize2, Trash2, Users } from "lucide-react";
+import { Maximize2, Minimize2, Plus, Trash2, Users } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useAtom } from "jotai";
+import { isGroupExistAtom, newGroupArrayAtom } from "@/states";
+import { Link } from "wouter";
 
-const userObj = z.object({
-  name: z.string(),
-});
+export default function Groups() {
+  const [newGroupArray, _] = useAtom(newGroupArrayAtom);
+  const [isGroupExist, setIsGroupExist] = useAtom(isGroupExistAtom);
+  useEffect(() => {
+    if (newGroupArray.length == 0) {
+      setIsGroupExist(false);
+    } 
+  }, [newGroupArray]);
 
-const formSchema = z.object({
-  groupname: z.string().min(3, {
-    message: "Group name must be at least 3 characters.",
-  }),
-  groupdescription: z
-    .string()
-    .max(30, {
-      message: "Description must be in the range of 30 characters.",
-    })
-    .or(z.literal("")),
-  groupmembers: z.array(userObj),
-});
-
-interface GroupFormProps {
-  setNewGroup: Function;
-  setIsGroupExist: Function;
-  setIsFormOpen: Function;
+  return (
+    <>
+      <div className="flex flex-col  h-lvh w-lvw bg-secondary border border-black">
+        <div className="h-[10%] bg-secondary p-4 flex items-start justify-between">
+          <h1 className="text-2xl font-semibold ">Groups</h1>
+          <Link href="/add/group">
+            <Plus className="border border-input bg-background hover:bg-accent hover:text-accent-foreground p-2 size-8 rounded-lg cursor-pointer" />
+          </Link>
+        </div>
+        <div className="p-4 rounded-t-3xl ml-auto mr-auto w-full h-[90%] bg-background relative">
+          {!isGroupExist && (
+            <h1 className="text-muted-foreground font-semibold text-base absolute top-[10%] left-1/2 transform -translate-x-1/2 -translate-y-[10%]">
+              No Groups Available
+            </h1>
+          )}
+          {isGroupExist && <GroupList />}
+        </div>
+      </div>
+    </>
+  );
 }
+
 interface GroupCardProps {
   group: {
     groupname: string;
@@ -51,10 +47,8 @@ interface GroupCardProps {
 
 //this is group card component it has delete,expand feature
 function GroupCard({ group, deleteGroup, deleteIndex }: GroupCardProps) {
-  console.log("group", group.groupname);
   const [isFullView, setIsFullView] = useState(false);
   function handleFullView() {
-    console.log("hie");
     setIsFullView((prev) => !prev);
   }
   function GroupCardFullview() {
@@ -83,8 +77,10 @@ function GroupCard({ group, deleteGroup, deleteIndex }: GroupCardProps) {
               );
             })}
           </div>
-          <Minimize2 onClick={() => handleFullView()}
-              className="w-5 cursor-pointer absolute right-2 top-2"/>
+          <Minimize2
+            onClick={() => handleFullView()}
+            className="w-5 cursor-pointer absolute right-2 top-2"
+          />
         </div>
       </div>
     );
@@ -92,19 +88,19 @@ function GroupCard({ group, deleteGroup, deleteIndex }: GroupCardProps) {
   return (
     <>
       {isFullView && <GroupCardFullview />}
-      <div className="m-4 rounded-lg border bg-div text-div-foreground shadow-sm flex items-center space-x-4 p-4">
+      <div className="w-[100%] rounded-2xl border bg-secondary text-div-foreground shadow-sm flex items-center space-x-4 p-4">
         <div className="flex-1">
-          <h1 className="text-2xl font-semibold leading-none tracking-tight">
+          <h1 className="text-lg font-medium leading-none tracking-tight">
             {group.groupname}
           </h1>
           <div className="flex justify-between mt-6 relative">
             <div className="flex">
-              <Users className="mr-1 w-5 cursor-pointer" />
+              <Users className="mr-1 w-4 cursor-pointer" />
               {group.groupmembers.length}
             </div>
-            <Maximize2 
+            <Maximize2
               onClick={() => handleFullView()}
-              className="w-5 cursor-pointer"
+              className="w-4 cursor-pointer"
             />
             <Trash2
               className="absolute right-8 w-5 text-destructive cursor-pointer"
@@ -117,171 +113,28 @@ function GroupCard({ group, deleteGroup, deleteIndex }: GroupCardProps) {
   );
 }
 
-//This is a group form component used for displayin form
-function GroupForm({
-  setNewGroup,
-  setIsGroupExist,
-  setIsFormOpen,
-}: GroupFormProps) {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      groupname: "",
-      groupdescription: "",
-      groupmembers: [],
-    },
-  });
-
-  const { control, handleSubmit } = form;
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: "groupmembers",
-  });
-
-  const handleAddMember = (memberName: string) => {
-    console.log("memberName: ", memberName);
-    append({ name: memberName });
-  };
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    setNewGroup((prev: any) => [...prev, values]);
-    setIsGroupExist(true);
-    setIsFormOpen(false);
-  }
-
-  return (
-    <>
-      <div className="fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 overflow-y-scroll mb-2">
-        <Form {...form}>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="rounded-lg border bg-card text-card-foreground shadow-sm m-4 relative space-y-8 p-4"
-          >
-            <div className="flex flex-col gap-2 group-info bg-secondary p-4 rounded-md">
-              <h1 className="font-semibold text-xl mb-1 ">Group Information</h1>
-              <FormField
-                control={control}
-                name="groupname"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Group name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Friends" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={control}
-                name="groupdescription"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Group description</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        placeholder="Describe relevant information about group"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            {/* members section */}
-            <div className="flex flex-col gap-2 bg-secondary p-4 rounded-md">
-              <h1 className="font-semibold text-xl mb-1 ">Group Members</h1>
-              <div className="">
-                {fields.map((member, index) => (
-                  <div
-                    key={member.id}
-                    className="shadow-sm rounded-lg bg-card text-card-foreground p-3 pl-5 flex justify-between mb-1"
-                  >
-                    <span className="text-sm">{member.name}</span>
-                    <Trash2
-                      className="w-5 text-destructive cursor-pointer"
-                      onClick={() => remove(index)}
-                    />
-                  </div>
-                ))}
-              </div>
-              <FormField
-                control={control}
-                name="groupmembers"
-                render={() => (
-                  <FormItem>
-                    <FormControl>
-                      <SearchBox onAddMember={handleAddMember} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <Button type="submit">Create</Button>
-            <Button
-              type="button"
-              variant="outline"
-              className="ml-4"
-              onClick={() => setIsFormOpen(false)}
-            >
-              Cancel
-            </Button>
-          </form>
-        </Form>
-      </div>
-    </>
-  );
-}
-
-interface Group {
-  groupname: string;
-  groupdescription: string;
-  groupmembers: string[];
-}
-export default function Groups() {
-  const [newGroup, setNewGroup] = useState<Group[]>([]);
-  const [isGroupExist, setIsGroupExist] = useState(false);
-  const [isFormOpen, setIsFormOpen] = useState(false);
-  // const []
-  useEffect(() => {
-    if (newGroup.length == 0) {
-      setIsGroupExist(false);
-    }
-    console.log("newGroup", isFormOpen);
-  }, [newGroup, isFormOpen]);
-
+function GroupList() {
+  const [newGroupArray, setNewGroupArray] = useAtom(newGroupArrayAtom);
   function handleDeleteGroup(index: number) {
-    const newReducedGroup = newGroup.filter((_, itemindex) => {
+    const newReducedGroup = newGroupArray.filter((_, itemindex) => {
       return itemindex !== index;
     });
-    setNewGroup(newReducedGroup);
-  }
-  function handleFormOpen() {
-    setIsFormOpen(true);
+    setNewGroupArray(newReducedGroup);
   }
   return (
-    <>
-      {isGroupExist &&
-        newGroup.map((group, index) => (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-muted-foreground font-semibold text-base">
+        Hot Groups
+      </h2>
+      <div className="p-0 grid grid-cols-2 gap-4  pt-4">
+        {newGroupArray.map((group, index) => (
           <GroupCard
             group={group}
             deleteIndex={index}
             deleteGroup={handleDeleteGroup}
           />
         ))}
-      {isFormOpen && (
-        <GroupForm
-          setNewGroup={setNewGroup}
-          setIsGroupExist={setIsGroupExist}
-          setIsFormOpen={setIsFormOpen}
-        />
-      )}
-      <Button variant="default" className="m-4" onClick={handleFormOpen}>
-        Open Form
-      </Button>
-    </>
+      </div>
+    </div>
   );
 }
