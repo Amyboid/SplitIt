56c67d3bbf3c7cee07682c3b73e44ba810a46c1c
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-11-13 20:32:24 +0530

    added heading and navigation

--- frontend/src/views/Notifications.tsx
+++ frontend/src/views/Notifications.tsx
@@ -1,7 +1,16 @@
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import { TypographyH3 } from "@/components/ui/typography-h3";
+import { ChevronLeft } from "lucide-react";
+
 export default function Notifications() {
-  return (
-    <>
-        
-    </>
-  )
+    return (
+        <>
+            <div className="flex selection:items-center mt-4 mx-4">
+                <span className="inline mt-[.37rem] mr-auto"><ChevronLeft /></span>
+                <TypographyH3 className="inline -ml-6 mr-auto">
+                    Notifications
+                </TypographyH3>
+            </div>
+        </>
+    )
 }
