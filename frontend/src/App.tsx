8265ca204b8e72cb59491ec502df3fb3d9833ecf
nextpointer <110530249+nextpointer@users.noreply.github.com>
2024-11-13 15:51:21 +0530

    Frontend : Fix a problem with tab changing with route not changing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     ["AddExpense", "/add/expense"],
     ["history", "/history"],
     ["expense", "/expense"],
-    ["auth","/auth/:path"]
+    ["auth","/auth/login"]
   ];
 
   return (
--- frontend/src/views/Auth.tsx
+++ frontend/src/views/Auth.tsx
@@ -8,21 +8,26 @@ import {
   } from "@/components/ui/card"
 import { CustomRegister } from "@/components/ui/CustomRegister";
 import { CustomLogin } from "@/components/ui/CustomLogin";
-import { useRoute } from "wouter";
+import { useRoute ,useLocation } from "wouter";
 
 
 
 export default function Auth() {
-  const [match,params]= useRoute("/auth/:path")
+  const [match,params]= useRoute("/auth/:path");
+  const [_location,setLocation]  =useLocation();
   const defualtValue  = match?params?.path:"register"
+
+  const handleTabChange = (path:string) => {
+    setLocation(`/auth/${path}`)
+  }
   
   return (
     <>
     <main className="w-full h-screen flex items-center justify-center pb-24">
       <Tabs defaultValue={defualtValue} className="w-[350px]">
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="register">Register</TabsTrigger>
-          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register" onClick={()=>handleTabChange("register")}>Register</TabsTrigger>
+          <TabsTrigger value="login" onClick={()=>handleTabChange("login")}>Login</TabsTrigger>
         </TabsList>
         <TabsContent value="register">
         <Card>
