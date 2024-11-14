8f0e3c420b525d0d4d24294979c4fcdb73038791
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-11-15 04:14:29 +0530

    refactor: made all these routes potected, added back functionality

--- frontend/src/views/Onboarding.tsx
+++ frontend/src/views/Onboarding.tsx
@@ -1,25 +1,18 @@
-import {
-  Form,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormDescription,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useLogto } from "@logto/react";
-import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Form, FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useLocation } from "wouter";
 
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
   firstname: z.string().min(2, {
-    message: "First name must be at least 2 characters.",
+    message: "First Name must be at least 2 characters.",
   }),
   lastname: z.string().min(2, {
     message: "Last name must be at least 2 characters.",
@@ -28,6 +21,7 @@ const formSchema = z.object({
 
 export default function Onboarding() {
   const { isAuthenticated } = useLogto();
+  const [, setLocation ] = useLocation();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -41,6 +35,8 @@ export default function Onboarding() {
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
   }
+
+  if (isAuthenticated) setLocation("/");
 
   return (
     <div>
