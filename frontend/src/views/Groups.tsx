fe286662f2472a297c36ff176bb406af15d1dacc
amyboid <134444582+Amyboid@users.noreply.github.com>
2024-11-15 02:22:56 +0530

    updated internal GroupCard component removed handleFullView instead added redirect functionality(not completed)

--- frontend/src/views/Groups.tsx
+++ frontend/src/views/Groups.tsx
@@ -1,5 +1,5 @@
-import { ArrowRight, Minimize2, Plus, Trash2, Users } from "lucide-react";
-import { useEffect, useState } from "react";
+import { ArrowRight, Plus, Users } from "lucide-react";
+import { useEffect } from "react";
 import { useAtom } from "jotai";
 import { isGroupExistAtom, newGroupArrayAtom } from "@/states";
 import { Link } from "wouter";
@@ -15,14 +15,14 @@ export default function Groups() {
 
   return (
     <>
-      <div className="flex flex-col  h-lvh w-lvw bg-secondary border border-black">
-        <div className="h-[10%] bg-secondary p-4 flex items-start justify-between">
-          <h1 className="text-2xl font-semibold ">Groups</h1>
+      <div className="flex flex-col  h-lvh w-lvw">
+        <div className="h-[30%] p-4 flex items-start justify-between bg-[url('/groupbg6.jpg')] bg-center bg-cover  bg-no-repeat">
+          <h1 className="text-2xl font-semibold text-black">Groups</h1>
           <Link href="/add/group">
-            <Plus className="border border-input bg-background hover:bg-accent hover:text-accent-foreground p-2 size-8 rounded-lg cursor-pointer" />
+            <Plus className="p-2 size-9 cursor-pointer text-black" />
           </Link>
         </div>
-        <div className="p-4 rounded-t-3xl ml-auto mr-auto w-full h-[90%] bg-background relative">
+        <div className="p-4  ml-auto mr-auto w-full h-[75%] bg-background relative">
           {!isGroupExist && (
             <h1 className="text-muted-foreground font-semibold text-base absolute top-[10%] left-1/2 transform -translate-x-1/2 -translate-y-[10%]">
               No Groups Available
@@ -34,7 +34,6 @@ export default function Groups() {
     </>
   );
 }
-
 
 function GroupList() {
   const [newGroupArray, setNewGroupArray] = useAtom(newGroupArrayAtom);
@@ -52,9 +51,9 @@ function GroupList() {
       <div className="p-0 grid grid-cols-2 gap-4  pt-4">
         {newGroupArray.map((group, index) => (
           <GroupCard
-          group={group}
-          deleteIndex={index}
-          deleteGroup={handleDeleteGroup}
+            group={group}
+            deleteIndex={index}
+            deleteGroup={handleDeleteGroup}
           />
         ))}
       </div>
@@ -73,49 +72,9 @@ interface GroupCardProps {
   deleteGroup: (index: number) => void;
 }
 function GroupCard({ group, deleteGroup, deleteIndex }: GroupCardProps) {
-  const [isFullView, setIsFullView] = useState(false);
-  function handleFullView() {
-    setIsFullView((prev) => !prev);
-  }
-  function GroupCardFullview() {
-    return (
-      <div className="fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 p-4 py-6 my-4 h-max">
-        <div className="z-50 w-72 rounded-md border bg-popover p-1 text-popover-foreground shadow-md outline-none  ml-auto mr-auto relative">
-          <div className="flex flex-col space-y-1.5 p-6">
-            <div className="text-2xl font-semibold leading-none tracking-tight">
-              {group.groupname}
-            </div>
-            <div className="text-sm text-muted-foreground">
-              {group.groupdescription.length > 0
-                ? group.groupdescription
-                : "no description"}
-            </div>
-          </div>
-          <div className="p-4 pt-0 gap-2 flex flex-wrap items-center">
-            {group.groupmembers.map((member: any, index) => {
-              return (
-                <div
-                  key={index}
-                  className="shadow-sm rounded-lg bg-secondary text-card-foreground text-sm p-2 "
-                >
-                  {member.name}
-                </div>
-              );
-            })}
-          </div>
-          <Minimize2
-            onClick={() => handleFullView()}
-            className="w-5 cursor-pointer absolute right-2 top-2"
-          />
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
-      {isFullView && <GroupCardFullview />}
-      <div className="w-full relative h-24 rounded-xl border bg-secondary text-div-foreground shadow-sm flex flex-col justify-between p-3 pb-2">
+      <div className="w-full relative h-24 rounded-xl bg-secondary text-div-foreground flex flex-col justify-between p-3 pb-2">
         <h1 className="text-lg font-medium leading-none tracking-tight">
           {group.groupname}
         </h1>
@@ -124,18 +83,17 @@ function GroupCard({ group, deleteGroup, deleteIndex }: GroupCardProps) {
             <Users className="mr-1 w-4 cursor-pointer" />
             {group.groupmembers.length}
           </div>
-          <div
-            onClick={() => handleFullView()}
-            className="flex items-start justify-center gap-1 mt-2"
-          >
-            <p className="text-xs font-semibold block leading-3">view</p>
-            <ArrowRight className="size-3 leading-3 font-bold text-chart-4" />
-          </div>
+          <Link href="/group/details">
+            <div className="flex items-start justify-center gap-1 mt-2">
+              <p className="text-xs font-semibold block leading-3">view</p>
+              <ArrowRight className="size-3 leading-3 font-bold text-chart-4" />
+            </div>
+          </Link>
         </div>
         {/* <Trash2
             className="absolute right-3 top-3 w-5 text-destructive cursor-pointer"
             onClick={() => deleteGroup(deleteIndex)}
-          /> */}
+            /> */}
       </div>
     </>
   );
