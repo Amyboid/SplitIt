6562f39aea095885aba88757e72cb81594e4d14c
nextpointer <110530249+nextpointer@users.noreply.github.com>
2024-11-14 16:05:58 +0530

    Frontend: Auth page done

--- frontend/src/views/Auth.tsx
+++ frontend/src/views/Auth.tsx
@@ -1,62 +1,21 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import { CustomRegister } from "@/components/ui/CustomRegister";
-import { CustomLogin } from "@/components/ui/CustomLogin";
-import { useRoute ,useLocation } from "wouter";
-
+import { useHandleSignInCallback } from "@logto/react";
+import { useLocation } from "wouter";
 
 
 export default function Auth() {
-  const [match,params]= useRoute("/auth/:path");
-  const [_location,setLocation]  =useLocation();
+  const [_location, setLocation] = useLocation();
 
-  // set the param value with the default value
-  const defualtValue  = match?params?.path:"register"
 
-  // route change with tab changing
-  const handleTabChange = (path:string) => {
-    setLocation(`/auth/${path}`)
+
+  const { isLoading } = useHandleSignInCallback(() => {
+    // Navigate to root path when finished
+    setLocation("/dashboard");
+  });
+
+  // When it's working in progress
+  if (isLoading) {
+    return <div className="h-screen w-full grid place-items-center">Redirecting...</div>;
   }
-  
-  return (
-    <>
-    <main className="w-full h-auto flex items-center justify-center mt-10">
-      <Tabs defaultValue={defualtValue} className="w-[350px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="register" onClick={()=>handleTabChange("register")}>Register</TabsTrigger>
-          <TabsTrigger value="login" onClick={()=>handleTabChange("login")}>Login</TabsTrigger>
-        </TabsList>
-        <TabsContent value="register">
-        <Card>
-          <CardHeader>
-            <CardTitle>Create Your Account</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <CustomRegister/>
-          </CardContent>
-        </Card>
-        </TabsContent>
-        <TabsContent value="login">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Join with us to fill your information
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <CustomLogin/>
-          </CardContent>
-        </Card>
-        </TabsContent>
-      </Tabs>
-      </main>
-    </>
-  );
+
+  return null;
 }
