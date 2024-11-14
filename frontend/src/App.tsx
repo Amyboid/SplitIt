1544982ae062c7df25ef21da809059d4f95986b7
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-11-15 02:22:31 +0530

    refactor App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,27 @@
 import "./App.css";
 import { ThemeProvider } from "./components/theme-provider"; 
 import { Route } from "wouter";
+import Home from "./views/Home";
 import Auth from "./views/Auth";
+import Onboarding from "./views/Onboarding";
 import Dashboard from "./views/Dashboard";
 import Notifications from "./views/Notifications";
 import Settings from "./views/Settings";
 import ExpenseInput from "./views/ExpenseInput";
-import Home from "./views/Home";
 
 
 
 function App() {
-
   return (
     <>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="preferred-ui-theme">
       <Route path="/" component={Home}/>
+      <Route path="/auth" component={Auth} />
+      <Route path="/onboarding" component={Onboarding} />
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/notification" component={Notifications} />
       <Route path="/settings" component={Settings} />
       <Route path="/add/expense" component={ExpenseInput} />
-      <Route path="/auth" component={Auth} />
     </ThemeProvider>
     </>
   );
