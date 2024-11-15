6da83611776e21ec995a41a0030401c4fe6065a4
amyboid <134444582+Amyboid@users.noreply.github.com>
2024-11-15 19:54:49 +0530

    added const file moved states to liv folder

---
+++ frontend/src/lib/const.ts
@@ -0,0 +1,7 @@
+const RedirectURI = "http://localhost:5173/auth/"
+const PSRedirectURI = "http://localhost:5173/"
+const LogtoEndpoint = "https://7wyo02.logto.app/"
+const LogtoAppId = "5g5383xkm0aacfs62bn7g"
+
+
+export { RedirectURI, PSRedirectURI, LogtoEndpoint, LogtoAppId }
