677082376f7fc830e05933260e6faef2994d176a
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2024-11-25 00:42:15 +0530

    created script asyncrunner to run functions that require top level await and isolated output

---
+++ backend/scripts/asyncrunner.ts
@@ -0,0 +1,11 @@
+import { deleteUsers } from "../src/lib/logto";
+
+// import and run async function here
+
+
+// (async () => await deleteUsers()) )();
+
+// (async () => {
+//     await deleteUsers()
+// })();
+
